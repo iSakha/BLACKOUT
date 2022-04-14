119bbe08016aa0fb3d15bd0c66958d415e7bbfc5
Alexander Omelianenko <sakha60@gmail.com>
2022-04-14 23:01:36 +0300

    Start with Equipment page

--- main_server/app.js
+++ main_server/app.js
@@ -8,9 +8,27 @@ app.use(express.urlencoded({ extended: false }));
 // создаем парсер для данных application/x-www-form-urlencoded
 const urlencodedParser = express.urlencoded({ extended: false });
 
-//      Get Home page
+//      Get Home page (Calendar)
 // ==========================================================
 app.use(express.static(__dirname + "/public"));
+
+
+//      Get Equipment page
+// ==========================================================
+app.get("/equip", function (request, response) {
+  response.sendFile(__dirname + "/public/equip.html");
+});
+
+
+
+//      Get Reservation page
+// ==========================================================
+app.get("/reserve", function (request, response) {
+  response.sendFile(__dirname + "/public/reserve.html");
+});
+
+
+
 
 
 app.listen(PORT, () => {
