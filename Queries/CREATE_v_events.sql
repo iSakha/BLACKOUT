1ab1dbcb1c27e365d4b7a7b3112783db691985eb
Alexander Omelianenko <sakha60@gmail.com>
2022-05-11 08:42:10 +0300

    some changes

--- Queries/CREATE_v_events.sql
+++ Queries/CREATE_v_events.sql
@@ -1,31 +1,48 @@
-CREATE VIEW `v_events_c` AS
-
 SELECT 
 `t_events`.`id` AS `id`,
 `t_events`.`calendarId` AS `calendarId`,
+`t_warehouses`.`warehouse` AS `warehouse`,
 `t_events`.`title` AS `title`,
-CAST(`t_events`.`start` AS DATE) AS `start`,
-CAST(`t_events`.`end` AS DATE) AS `end`,
-`t_events`.`notes` AS `notes`,
-CONCAT(`t_event_location`.`city`, ', ',`t_event_location`.`place`) AS `location`,
-`t_calendars`.`cal_name` AS `cal_name`,
+`t_clients`.`id` AS `clientId`,
+`t_clients`.`client` AS `client`,
+cast(`t_events`.`start` as date) AS `start`,
+cast(`t_events`.`end` as date) AS `end`,
+`t_events`.`status` AS `statusId`,
 `t_event_status`.`status` AS `status`,
-`t_events`.`phase` AS `phase`,
-CONCAT(`m1`.`f_name`, ', ',`m1`.`s_name`, `m1`.`l_name`) AS `manager_1`,
-CONCAT(`m2`.`f_name`, ', ',`m2`.`s_name`, `m2`.`l_name`) AS `manager_2`
+`b_out`.`t_events`.`location_city` AS `event_cityId`,
+`b_out`.`t_event_location_city`.`event_city` AS `event_city`,
+`b_out`.`t_events`.`location_place` AS `event_placeId`,
+`b_out`.`t_event_location_place`.`event_place` AS `event_place`,
+`b_out`.`t_events`.`notes` AS `notes`,
+`b_out`.`t_events`.`manager_1` AS `manager_1Id`,
+concat(`m1`.`f_name`,', ',`m1`.`s_name`,`m1`.`l_name`) AS `manager_1`,
+`b_out`.`t_events`.`manager_2` AS `manager_2Id`,
+concat(`m2`.`f_name`,', ',`m2`.`s_name`,`m2`.`l_name`) AS `manager_2`,
+`b_out`.`t_events`.`current_user` AS `current_user` 
 
+FROM 
 
- 
-FROM `t_events` 
-JOIN `t_calendars` 
-ON `t_events`.`calendarId` = `t_calendars`.`id`
-JOIN `t_event_status`
-ON `t_events`.`status` = `t_event_status`.`id`
-JOIN `t_event_location`
-ON `t_events`.`location` = `t_event_location`.`id`
-JOIN `t_managers` AS `m1`
-ON `t_events`.`manager_1` = `m1`.`id`
-JOIN `t_managers` AS `m2`
-ON `t_events`.`manager_2` = `m2`.`id`
+`t_events` 
 
-WHERE `t_events`.is_deleted = 0
+JOIN `t_warehouses` 
+ON `b_out`.`t_events`.`calendarId` = `b_out`.`t_warehouses`.`id`
+
+JOIN `t_event_status` 
+ON `b_out`.`t_events`.`status` = `b_out`.`t_event_status`.`id`
+
+join `t_managers` AS `m1` 
+ON `b_out`.`t_events`.`manager_1` = `m1`.`id` 
+
+JOIN `t_managers` AS `m2` 
+ON `b_out`.`t_events`.`manager_2` = `m2`.`id` 
+
+JOIN `t_event_location_city` 
+ON `b_out`.`t_events`.`location_city` = `b_out`.`t_event_location_city`.`id` 
+
+JOIN `t_event_location_place` 
+ON `b_out`.`t_events`.`location_place` = `b_out`.`t_event_location_place`.`id`
+
+JOIN `t_clients` 
+ON `b_out`.`t_events`.`client` = `b_out`.`t_clients`.`id`
+
+WHERE `b_out`.`t_events`.`is_deleted` = 0
