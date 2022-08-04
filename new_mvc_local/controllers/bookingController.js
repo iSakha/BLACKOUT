8aaa93d959ec7516c119965cef166bb719cccd7b
iSakha <sakha60@gmail.com>
2022-08-04 22:39:25 +0300

    start with booking calendar

--- new_mvc_local/controllers/bookingController.js
+++ new_mvc_local/controllers/bookingController.js
@@ -1,5 +1,7 @@
 const jwt = require('jsonwebtoken');
 const BookedEquip = require('../models/bookingModel');
+const Event = require('../models/eventModel');
+const Phase = require('../models/phaseModel');
 const utils = require('../utils/utils');
 const auth = require('../controllers/authController');
 
@@ -19,7 +21,7 @@ exports.setBookedModels = async (req, res) => {
 
         console.log("authentication successfull!");
         let obj = {};
-        console.log("req.body[0].model:",req.body[0].model);
+        console.log("req.body[0].model:", req.body[0].model);
         for (let i = 0; i < req.body[0].model.length; i++) {
             console.log("test");
             // let obj = new EventEquip(req.body[i].idEvent, req.body[i].idFixture, req.body[i].requiredQty, req.body[i].idWarehouse, req.body[i].whsQty);
@@ -34,7 +36,7 @@ exports.setBookedModels = async (req, res) => {
             obj.userId = userId;
             obj.unixTime = unixTime;
             const objRow = Object.values(obj);
-            
+
 
             dataRow.push(objRow);
         }
@@ -44,6 +46,24 @@ exports.setBookedModels = async (req, res) => {
         try {
             const [bookedEquip] = await BookedEquip.setBookedModels(dataRow);
             console.log("bookedEquip:", bookedEquip);
+
+            try {
+                const [event] = await Event.getOne(req.body[0].event.id);
+                console.log("event:", event);
+                // [phases] = await Phase.getOnePhase(req.params.id);
+                // console.log("phases:", phases);
+                updateBookingCalendar(res, event[0].start.toISOString().slice(0, 10), event[0].end.toISOString().slice(0, 10));
+
+            } catch (error) {
+                console.log("error:", error);
+                res.status(500).json({ msg: "We have problems with getting event from database" });
+                return {
+                    error: true,
+                    message: 'Error from database'
+                }
+            }
+
+
         } catch (error) {
             console.log("error:", error);
             res.status(500).json({ msg: "We have problems with writing booked equipment to database" });
@@ -53,11 +73,25 @@ exports.setBookedModels = async (req, res) => {
             }
         }
 
-        res.status(200).json({ msg: `Оборудование для мероприятия выбрано.` });
+
 
         // } else res.status(400).json(msg);
 
     } else {
         res.status(status.status).json({ msg: "We have problems with JWT authentication" });
     }
+}
+
+updateBookingCalendar = async (res, dateStart, dateEnd) => {
+
+    console.log("updateBookingCalendar");
+    console.log("start:", dateStart);
+    console.log("end:", dateEnd);
+
+    const diffInMs = new Date(dateEnd) - new Date(dateStart)
+    const eventDays = diffInMs / (1000 * 60 * 60 * 24) + 1;
+
+    console.log("eventDays :", eventDays );
+
+    res.status(200).json({ msg: `Оборудование для мероприятия выбрано.` });
 }
--- new_mvc_local/models/bookingModel.js
+++ new_mvc_local/models/bookingModel.js
@@ -12,14 +12,6 @@ module.exports = class BookedEquip {
     return db.query('INSERT INTO `t_event_equipment` (idEvent, idFixture, requiredQty, idWarehouse, whsQty, idUser, unixTime) VALUES ?', [dataRow]);
  }   
 
-static setBookModels(dataRow) {
-    try {
-        return db.query('INSERT INTO `t_event_equipment` (idEvent, idFixture, requiredQty, idWarehouse, whsQty, idUser, unixTime) VALUES ?', [dataRow]);
-    } catch (error) {
-        return error;
-    }
-}
-
 static bookFixtures() {
     
 }
